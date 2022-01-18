66ca2751a2f9319bbe48e26d08ffac7b417a42c9
Chiba <handball89@outlook.jp>
2022-01-18 17:09:52 +0900

    mail

--- issue01.py
+++ issue01.py
@@ -13,8 +13,12 @@ def search():
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
     
     ### ここに検索ロジックを書く
+    if word in source:
+        print("{}が見つかりました".format(word))
+    else:
+        print("{}が見つかりませんでした".format(word))
+    
 
-    print("{}が見つかりした".format(word))
 
 if __name__ == "__main__":
     search()
