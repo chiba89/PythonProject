3f25dfeeea3e5a54d7db62a24d9bfe0fbdabee51
Chiba <handball89@outlook.jp>
2022-01-25 15:10:10 +0900

    add2

--- issue2.py
+++ issue2.py
@@ -45,6 +45,7 @@
 # ## ８　難易度★☆☆☆☆
 # Chromeドライバーがバージョンアップの際に自動で更新されるようにしてみましょう。  
 # 参考：https://qiita.com/YoshikiIto/items/000f241f6d917178981c
+    # driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # 以下は、オプション課題です。少し難易度が高いのでトライしたい方のみ<BR>
 # 挑戦してみてください。
@@ -53,105 +54,142 @@
 # 検索時等にWeb画面を更新する処理はurlにより制御されます。
 # そのため、検索窓を使用せずにURLを直接変更することでも検索結果を取得することが可能です。
 
-
 #########
 
-# import os
-# from selenium.webdriver import Chrome, ChromeOptions
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
-# from webdriver_manager.chrome import ChromeDriverManager
-# import time
-# import pandas as pd
+from asyncio.windows_events import NULL
+import os
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import pandas as pd
 
-# # Selenium4対応済
+# Selenium4対応済
 
+def set_driver(hidden_chrome: bool=False):
+    '''
+    Chromeを自動操作するためのChromeDriverを起動してobjectを取得する
+    '''
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    options = ChromeOptions()
 
-# def set_driver(hidden_chrome: bool=False):
-#     '''
-#     Chromeを自動操作するためのChromeDriverを起動してobjectを取得する
-#     '''
-#     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-#     options = ChromeOptions()
+    # ヘッドレスモード（画面非表示モード）をの設定
+    if hidden_chrome:
+        options.add_argument('--headless')
 
-#     # ヘッドレスモード（画面非表示モード）をの設定
-#     if hidden_chrome:
-#         options.add_argument('--headless')
-
-#     # 起動オプションの設定
-#     options.add_argument(f'--user-agent={USER_AGENT}') # ブラウザの種類を特定するための文字列
-#     options.add_argument('log-level=3') # 不要なログを非表示にする
-#     options.add_argument('--ignore-certificate-errors') # 不要なログを非表示にする
-#     options.add_argument('--ignore-ssl-errors') # 不要なログを非表示にする
-#     options.add_experimental_option('excludeSwitches', ['enable-logging']) # 不要なログを非表示にする
-#     options.add_argument('--incognito') # シークレットモードの設定を付与
+    # 起動オプションの設定
+    options.add_argument(f'--user-agent={USER_AGENT}') # ブラウザの種類を特定するための文字列
+    options.add_argument('log-level=3') # 不要なログを非表示にする
+    options.add_argument('--ignore-certificate-errors') # 不要なログを非表示にする
+    options.add_argument('--ignore-ssl-errors') # 不要なログを非表示にする
+    options.add_experimental_option('excludeSwitches', ['enable-logging']) # 不要なログを非表示にする
+    options.add_argument('--incognito') # シークレットモードの設定を付与
     
-#     # ChromeのWebDriverオブジェクトを作成する。
-#     service=Service(ChromeDriverManager().install())
-#     return Chrome(service=service, options=options)
+    # ChromeのWebDriverオブジェクトを作成する。
+    service=Service(ChromeDriverManager().install())
+    return Chrome(service=service, options=options)
 
-
-# def main():
-#     '''
-#     main処理
-#     '''
-#     search_keyword = "高収入"
-#     # driverを起動
-#     driver = set_driver()
+def main():
+    '''
+    main処理
+    '''
+    search_keyword = input("検索キーワードを入力してください >>> ")
+    # driverを起動
+    driver = set_driver()
     
-#     # Webサイトを開く
-#     driver.get("https://tenshoku.mynavi.jp/")
-#     time.sleep(5)
+    # Webサイトを開く
+    driver.get("https://tenshoku.mynavi.jp/")
+    time.sleep(3)
     
-#     '''
-#     ポップアップを閉じる
-#     ※余計なポップアップが操作の邪魔になる場合がある
-#       モーダルのようなポップアップ画面は、通常のclick操作では処理できない場合があるため
-#       excute_scriptで直接Javascriptを実行することで対処する
-#     '''
-#     driver.execute_script('document.querySelector(".karte-close").click()')
-#     time.sleep(5)
-#     # ポップアップを閉じる
-#     driver.execute_script('document.querySelector(".karte-close").click()')
+    '''
+    ポップアップを閉じる
+    ※余計なポップアップが操作の邪魔になる場合がある
+      モーダルのようなポップアップ画面は、通常のclick操作では処理できない場合があるため
+      excute_scriptで直接Javascriptを実行することで対処する
+    '''
+    driver.execute_script('document.querySelector(".karte-close").click()')
+    time.sleep(3)
+    # ポップアップを閉じる
+    driver.execute_script('document.querySelector(".karte-close").click()')
 
+    '''
+    find_elementでHTML要素(WebElement)を取得する
+    byで、要素を特定する属性を指定するBy.CLASS_NAMEの他、By.NAME、By.ID、By.CSS_SELECTORなどがある
+    特定した要素に対して、send_keysで入力、clickでクリック、textでデータ取得が可能
+    '''
+    # 検索窓に入力
+    driver.find_element(by=By.CLASS_NAME, value="topSearch__text").send_keys(search_keyword)
+    # 検索ボタンクリック
+    driver.find_element(by=By.CLASS_NAME, value="topSearch__button").click()
 
-#     '''
-#     find_elementでHTML要素(WebElement)を取得する
-#     byで、要素を特定する属性を指定するBy.CLASS_NAMEの他、By.NAME、By.ID、By.CSS_SELECTORなどがある
-#     特定した要素に対して、send_keysで入力、clickでクリック、textでデータ取得が可能
-#     '''
-#     # 検索窓に入力
-#     driver.find_element(by=By.CLASS_NAME, value="topSearch__text").send_keys(search_keyword)
-#     # 検索ボタンクリック
-#     driver.find_element(by=By.CLASS_NAME, value="topSearch__button").click()
-
-
-#     '''
-#     find_elements(※複数形)を使用すると複数のデータがListで取得できる
-#     一覧から同一条件で複数のデータを取得する場合は、こちらを使用する
-#     '''
-#     name_elms = driver.find_elements(by=By.CLASS_NAME, value="cassetteRecruit__name")
+    '''
+    find_elements(※複数形)を使用すると複数のデータがListで取得できる
+    一覧から同一条件で複数のデータを取得する場合は、こちらを使用する
+    '''
+    # 空のDataFrame作成
+    df = pd.DataFrame()
     
-#     # 空のDataFrame作成
-#     df = pd.DataFrame()
+    count = 1
+    
+    while True:
+        recruit_elms = driver.find_elements(by=By.CSS_SELECTOR,value=".cassetteRecruit__content")
+    
+        for recruit_elm in recruit_elms:
+            
+            try:
+                name =  recruit_elm.find_element(by=By.CSS_SELECTOR,value=".cassetteRecruit__name").text
+                copy =  recruit_elm.find_element(by=By.CSS_SELECTOR,value=".cassetteRecruit__copy").text
+                
+                df = df.append({
+                    "会社名": name, 
+                    "タイトル": copy,
+                    # "項目C": ""
+                    }, 
+                    ignore_index=True)
+                
+                
+                print(str(count)+"件目成功")
+                # f = open('log2.txt', 'w', encoding='utf-8-sig')
+                # f.write("{count}件目成功")
+                # f.writelines(str(count)+"件目成功\n")
+                # f.close()
 
-#     # 1ページ分繰り返し
-#     print(len(name_elms))
-#     '''
-#     name_elmsには１ページ分の情報が格納されているのでforでループさせて１つづつ取り出して、Dataframeに格納する
-#     '''
-#     for name_elm in name_elms:
-#         print(name_elm.text)
-#         # DataFrameに対して辞書形式でデータを追加する
-#         df = df.append(
-#             {"会社名": name_elm.text, 
-#              "項目B": "",
-#              "項目C": ""}, 
-#             ignore_index=True)
-        
-        
+                # datalist = ['お元気ですか？\n', 'それではまた\n']
 
+            
+            except Exception as e:
+                print(str(count)+"件目失敗")
+                # f = open('log2.txt', 'w', encoding='utf-8-sig')
+                # f.write("{count}件目失敗")
+                # f.writelines(str(count)+"件目失敗\n")
+                # f.close()
+            
+            finally:
+                count += 1
+                
+        nextpage = driver.find_elements(by=By.CSS_SELECTOR,value=".iconFont--arrowLeft")
 
-# # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
-# if __name__ == "__main__":
-#     main()
+        if nextpage != []:
+            driver.execute_script("arguments[0].click();", nextpage[0])
+            
+        else:
+            break
+            
+    print(df)
+    
+    df.to_csv('test2.csv',index=False, encoding='utf-8-sig')
+
+    # 1ページ分繰り返し
+    # print(len(name_elms))
+    '''
+    name_elmsには１ページ分の情報が格納されているのでforでループさせて１つづつ取り出して、Dataframeに格納する
+    '''
+    # for name_elm in name_elms:
+        # DataFrameに対して辞書形式でデータを追加する
+    
+    # for copy_elm in copy_elms:
+
+# 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
+if __name__ == "__main__":
+    main()
